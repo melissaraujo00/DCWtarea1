5dc2ea954396e787b2dfc4c1e474365082bcfcc0
melissaraujo00 <daliarivas11@gmail.com>
2025-03-08 02:25:53 -0600

    configuracion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}",  // Incluye todas las rutas de tus archivos HTML y JS donde usas Tailwind
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
